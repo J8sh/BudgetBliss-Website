1708b7ba27b5f4b149920e8cbaa624161a475b06
Joshua Garcia <j8shigarcia@gmail.com>
2026-04-17 17:54:23 -0500

    update auth again

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-export { auth as middleware } from "@/lib/auth/auth";
-
-export const config = {
-  matcher: [
-    // Protect all routes except login, static files, and NextAuth internals
-    "/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)",
-  ],
-};
